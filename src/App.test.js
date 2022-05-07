54921e896bca962f4389b7c68a0a4394bb7db869
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-05-07 20:50:33 +0530

    video 02 : first simple unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,11 @@
+import { add } from './App';
+
 test('Fake test', () => {
 	expect(true).toBeTruthy();
+});
+
+// Unit test
+test('add', () => {
+	const value = add(1, 2);
+	expect(value).toBe(3);
 });
