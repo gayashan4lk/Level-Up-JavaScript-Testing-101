48ae22c200ad50d46c94bbd0c3c5c0cf81899d09
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-05-08 09:59:51 +0530

    mocking a function

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,27 @@
-import { getSumOfTwoNumbers, getTotal } from './App';
+//import { getSumOfTwoNumbers, getTotal } from './App';
+import { getTotal } from './App';
 
-test('Fake test', () => {
-	expect(true).toBeTruthy();
-});
+// test('Fake test', () => {
+// 	expect(true).toBeTruthy();
+// });
 
-// Unit test
-test('getSumOfTwoNumbers test', () => {
-	const value = getSumOfTwoNumbers(1, 2);
-	expect(value).toBe(3);
-});
+// // Unit test
+// test('getSumOfTwoNumbers test', () => {
+// 	const value = getSumOfTwoNumbers(1, 2);
+// 	expect(value).toBe(3);
+// });
 
-test('getTotal test', () => {
-	const expectedValue = getTotal(20, 5);
-	expect(expectedValue).toBe('$25');
+// test('getTotal test', () => {
+// 	const expectedValue = getTotal(20, 5);
+// 	expect(expectedValue).toBe('$25');
+// });
+
+// Mocking
+const getSumOfTwoNumbers = jest.fn(() => 3);
+
+test('getSumofTwoNumbers Mocking test', () => {
+	const expectedValue = getSumOfTwoNumbers(1, 2);
+	expect(expectedValue).toBe(3);
+	expect(getSumOfTwoNumbers).toHaveBeenCalledTimes(1);
+	expect(getSumOfTwoNumbers).toHaveBeenCalledWith(1, 2);
 });
